5e302220de682389d10ef3e0ec1e77c69b46f07c
manit989 <manit17102006@gmail.com>
2026-01-09 17:37:02 +0530

    improved ui

--- src/App.jsx
+++ src/App.jsx
@@ -5,57 +5,71 @@ import "./App.css";
 function App() {
   const [url, setUrl] = useState("");
   const [method,setMethod] = useState("GET");
-  const [headers,setHeaders] = useState([]);
-  const [body,setBody] = useState("");
-  const [response,setResponse] = useState("");
+  const [headers,setHeaders] = useState({});
+  const [body,setBody] = useState("{}");
+  const [response,setResponse] = useState({meow:1});
 
   return (
     <div className="flex flex-col p-8">
       <Title/>
       <div className="flex flex-row gap-40 p-10">
-        <RequestForm/>
+        <RequestForm url={url} setUrl={setUrl} method={method} setMethod={setMethod} headers={headers} setHeaders={setHeaders} body={body} setBody={setBody}/>
         <Response response={response}/>
       </div>
     </div>
   );
 }
 
-function RequestForm() {
+function RequestForm(props) {
+  const [headerKey,setHeaderKey] = useState("");
+  const [headerValue,setHeaderValue] = useState("");
+
+  function addHeader() {
+          const newHeaders = {...(props.headers),[headerKey]:headerValue};
+          props.setHeaders(newHeaders);
+  }
+
   return (
-    <div className="bg-black flex flex-col gap-4 p-4">
+    <div className="bg-black flex flex-col gap-4 p-4 border-4 border-white rounded-md">
       <p className="text-xl text-white">URL</p>
-      <input name="method" type="text" className="bg-white"/>
+      <input name="method" type="text" className="bg-white" value={props.url} onChange={(e)=>props.setUrl(e.target.value)}/>
       <div>
-        <input name="method" type="radio" className="accent-blue-500" checked/>
-        <label for="phone" className="text-white text-s"> GET</label>
+        <input name="method" type="radio" className="accent-blue-500" />
+        <label for="phone" className="text-white text-s" onClick={()=>props.setMethod("GET")}> GET</label>
       </div>
       <div>
         <input name="method" type="radio" className="accent-blue-500"/>
-        <label for="phone" className="text-white text-s"> POST</label>
+        <label for="phone" className="text-white text-s" onClick={()=>props.setMethod("POST")}> POST</label>
       </div>
       <div>
         <input name="method" type="radio" className="accent-blue-500"/>
-        <label for="phone" className="text-white text-s"> PUT</label>
+        <label for="phone" className="text-white text-s" onClick={()=>props.setMethod("PUT")}> PUT</label>
       </div>
       <div>
         <input name="method" type="radio" className="accent-blue-500"/>
-        <label for="phone" className="text-white text-s"> DELETE</label>
+        <label for="phone" className="text-white text-s" onClick={()=>props.setMethod("DELETE")}> DELETE</label>
       </div>
       <div>
         <p className="text-xl text-white">Headers</p>
-        <input type="text" className="bg-white m-1" placeholder=" key"/>
-        <input type="text" className="bg-white m-1" placeholder=" value"/>
-        <button className="bg-gray-500 hover:bg-gray-400 text-white text-xs m-1 py-1 px-1">Add header</button>
+        <input type="text" className="bg-white m-1" value={headerKey} onChange={(e)=>setHeaderKey(e.target.value)}/>
+        <input type="text" className="bg-white m-1" value={headerValue} onChange={(e)=>setHeaderValue(e.target.value)}/>
+        <button className="bg-gray-500 hover:bg-gray-400 text-white text-xs m-1 py-1 px-1" onClick={()=>addHeader()}>Add header</button>
       </div>
       <p className="text-xl text-white">Body</p>
-      <textarea name="comments" className="bg-white" rows="4" cols="50" placeholder="{}"></textarea>
+      <textarea className="bg-white" rows="4" cols="50" value={props.body} onChange={(e)=>props.setBody(e.target.value)}></textarea>
+      <button className="bg-gray-500" >Submit</button>
     </div>
   );
 }
 
 function Response(props) {
   return (
-    <textarea name="comments" className="bg-white" rows="4" cols="50" placeholder={props.response}></textarea>
+          <div className="flex flex-col gap-4 p-4 border-4 border-white rounded-md">
+            <p className="text-2xl text-white">Response.json</p>
+            <pre className="text-white">
+                {JSON.stringify(props.response)}
+            </pre>
+          </div>
   );
 }
 
